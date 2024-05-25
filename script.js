66902296a83bac5b85c0f694760ee63833098ff2
k1tk4t5 <akemi90638@gmail.com>
2024-05-25 10:15:44 -0700

    adding placeholder cat form

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Get the value of the name input from the form and update the greeting
+let cat_form = $("#cat_form");
 
 const greetingText = document.querySelector("#greeting");                   // get greeting element
 const inputElement = document.querySelector("#exampleFormControlInput1");   // get input element
@@ -7,19 +8,29 @@ btn.addEventListener('click', function() {                                  // a
     greetingText.innerHTML = `Hello, ${inputElement.value}`;                // this is a templated string in Javscript! https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals 
 });
 
-const apiUrl = "https://cataas.com/cat?json=true";
-const outputElement = document.getElementById('testing');
-fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-    return response.json();
-})
-.then(data => {
-    console.log(data["_id"])
-    outputElement.src = "https://cataas.com/cat/" + data['_id'];
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+
+function submitCatForm(formSubmitEvent) {
+    console.log("submit cat form");
+
+    formSubmitEvent.preventDefault();
+
+    const apiUrl = "https://cataas.com/cat?json=true";
+    const catImage = document.getElementById('cat');
+    const catFormError = document.getElementById('cat_error_message');
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data["_id"])
+        catImage.src = "https://cataas.com/cat/" + data['_id'];
+    })
+    .catch(error => {
+        catFormError.textContent = 'Error:' + error;
+    });
+}
+
+cat_form.submit(submitCatForm);
