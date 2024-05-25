c8faa7184fe5eff1a237181079b0918a006fddf1
k1tk4t5 <akemi90638@gmail.com>
2024-05-25 10:36:55 -0700

    Removing all the starter stuff

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 // Get the value of the name input from the form and update the greeting
 let cat_form = $("#cat_form");
 
-const greetingText = document.querySelector("#greeting");                   // get greeting element
-const inputElement = document.querySelector("#exampleFormControlInput1");   // get input element
-const btn = document.querySelector("#exampleFormBtn");                      // get the element with id = 'exampleFormBtn'
-btn.addEventListener('click', function() {                                  // add an event to the button: whenever the button is pressed, update the greeting name
-    greetingText.innerHTML = `Hello, ${inputElement.value}`;                // this is a templated string in Javscript! https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals 
-});
-
-
 function submitCatForm(formSubmitEvent) {
     console.log("submit cat form");
 
     formSubmitEvent.preventDefault();
 
     const apiUrl = "https://cataas.com/cat?json=true";
-    const catImage = document.getElementById('cat');
+    const catImage = document.getElementById('cat-image');
     const catFormError = document.getElementById('cat_error_message');
     fetch(apiUrl)
         .then(response => {
@@ -26,6 +18,10 @@ function submitCatForm(formSubmitEvent) {
     })
     .then(data => {
         console.log(data["_id"])
+
+        const catText = document.querySelector('#cat-text');
+        console.log(catText.textContent);
+
         catImage.src = "https://cataas.com/cat/" + data['_id'];
     })
     .catch(error => {
